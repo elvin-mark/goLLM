c9d014a9092590944fbe0e9dada6f12d4afe0d34
Elvin Mark Munoz Vega <munoz.vega.em@gmail.com>
2023-12-24 11:55:37 +0900

    updating README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # go LLM
 
 Simple Implementation in GoLang to make inference of LLM models. No dependencies needed.
+
+# How to run
+
+Convert the weights of GPT2 to a binary file. You can use the following python script.
+
+```sh
+GPT2_MODEL_PATH=<PATH_TO_MODEL> GPT2_CONVERTED_MODEL_PATH=<PATH_TO_CONVERTED_MODEL> python res/converter.py
+```
+
+To start generating, run the following command.
+
+```sh
+GPT2_CONVERTED_MODEL_PATH=<PATH_TO_CONVERTED_MODEL> GPT2_TOKENIZER_PATH=<PATH_TO_TOKENIZER> go run main.go "hello my name is "
+```
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "github.com/elvin-mark/goLLM/tests"
+import (
+	"fmt"
+	"os"
+
+	"github.com/elvin-mark/goLLM/models"
+)
 
 func main() {
-	// tests.TestTensor()
-	// tests.TestLayers()
-	tests.TestModels()
+	fmt.Println("Loading tokenizer ...")
+	encoder := models.NewEncoder(os.Getenv("GPT2_TOKENIZER_PATH"))
+	fmt.Println("Encoding prompt ...")
+	tokens := encoder.Encode(os.Args[1])
+	fmt.Println("prompt tokens: ", tokens)
+
+	fmt.Println("Creating model ...")
+	conf := models.NewGPT2Config()
+	gpt := models.NewGPT2(conf)
+	fmt.Println("Initializing model ...")
+	gpt.Init()
+	fmt.Println("Loading weights")
+	gpt.LoadWeights(os.Getenv("GPT2_CONVERTED_MODEL_PATH"))
+	fmt.Println("Generating tokens ...")
+	seq := gpt.Generate(tokens, 10)
+	fmt.Println("generated tokens: ", seq)
+	fmt.Println(encoder.Decode(seq))
 }
